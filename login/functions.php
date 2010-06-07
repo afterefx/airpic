a76ecbbc351bba850abd491f619b9c11c4e3e608
Christopher Carlisle <carlic578@gmail.com>
2010-06-06 17:15:55 -0700

    Organized functions and added a few

--- login/functions.php
+++ login/functions.php
@@ -1,33 +1,5 @@
 <?php
 
-function connect()
-{
-    global $url, $userName, $password, $database;
-
-      if (($connection = mysql_connect($url, $userName, $password)) === FALSE)
-                  die("Could not connect to database");
-
-          // select database
-          if (mysql_select_db($database, $connection) === FALSE)
-                      die("Could not select database");
-}
-
-function redirect($navigateTo)
-{
-    $host = $_SERVER["HTTP_HOST"];
-    $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
-    $goto = sprintf("Location: http://%s%s/%s", $host, $path, $navigateTo);
-    header($goto);
-}
-
-function generateApiKey()
-{
-    mt_srand();
-    $number = mt_rand()+time();
-    $temp = hash("sha512",$number);
-    return substr($temp, 0, 40);
-}
-
 function checkForSession()
 {
     
@@ -65,6 +37,73 @@ function checkForSession()
     else
         return false;
 
+}
+
+function connect()
+{
+    global $url, $userName, $password, $database;
+
+      if (($connection = mysql_connect($url, $userName, $password)) === FALSE)
+                  die("Could not connect to database");
+
+          // select database
+          if (mysql_select_db($database, $connection) === FALSE)
+                      die("Could not select database");
+}
+
+function generateApiKey()
+{
+    mt_srand();
+    $number = mt_rand()+time();
+    $temp = hash("sha512",$number);
+    return substr($temp, 0, 40);
+}
+
+function getUserName()
+{
+    $getUserSQL = sprintf("SELECT user FROM session WHERE token='%s'",
+        mysql_real_escape_string($_SESSION["token"]));
+
+    $result = mysql_query($getUserSQL);
+
+    $row = mysql_fetch_array($result);
+    return $row['user'];
+}
+
+function getName()
+{
+    $user = getUserName();
+    
+    $sql = sprintf("SELECT fname FROM users WHERE user='%s'", $user);
+    $result = mysql_query($sql);
+
+    $row = mysql_fetch_array($result);
+
+    return $row['fname'];
+}
+
+
+function isAdmin()
+{
+    $user = getUserName();
+    
+    $sql = sprintf("SELECT isAdmin FROM users WHERE user='%s'", $user);
+    $result = mysql_query($sql);
+
+    $row = mysql_fetch_array($result);
+
+    if($row['isAdmin'])
+        return true;
+    else
+        return false;
+}
+
+function redirect($navigateTo)
+{
+    $host = $_SERVER["HTTP_HOST"];
+    $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+    $goto = sprintf("Location: http://%s%s/%s", $host, $path, $navigateTo);
+    header($goto);
 }
 
 function updateLastSeen()
